5ef988e1247c5e907f600c48452d95f822d8aad0
Dromediansk <pillar.mr@gmail.com>
2018-11-16 11:47:50 +0100

    changes before deploying

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://secure-shelf-99695.herokuapp.com/image', {
+        fetch('http://localhost:3000/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
