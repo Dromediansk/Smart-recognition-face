935260b5cca5c93305594ca88de727520216c074
Dromediansk <pillar.mr@gmail.com>
2019-03-05 18:19:13 +0100

    updating packages

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin/', {
+		fetch('https://immense-inlet-60412.herokuapp.com/signin/', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
