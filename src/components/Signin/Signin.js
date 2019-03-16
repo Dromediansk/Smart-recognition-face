f0e60f56ea7b1c03684f53d6650b7baf95ff24be
Dromediansk <pillar.mr@gmail.com>
2019-03-16 09:49:21 +0100

    changing links for development purposes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -25,7 +25,9 @@ class Signin extends React.Component {
 	onSubmitSignIn = () => {
 		fetch('http://localhost:3000/signin/', {
 			method: 'post',
-			headers: {'Content-Type': 'application/json'},
+			headers: {
+				'Content-Type': 'application/json'
+			},
 			body: JSON.stringify({
 				email: this.state.signInEmail,
 				password: this.state.signInPassword
